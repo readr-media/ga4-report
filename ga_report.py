e3698335a6290b0908117f39844b28bf85225b8c
hcchien <hcchien@gmail.com>
2022-12-15 12:30:03 +0800

    feat: modify the upload method by uploading stream

--- ga_report.py
+++ ga_report.py
@@ -41,14 +41,29 @@ def popular_report(property_id):
     response = client.run_report(request)
 
     print("Report result:")
-    with open('./popular.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        for row in response.rows:
-            writer.writerow([row.dimension_values[0].value])
-            #writer.writerow([row.dimension_values[0].value, row.dimension_values[1].value.encode('utf-8'), row.metric_values[0].value])
+    report = []
+    for row in response.rows:
+        #writer.writerow([row.dimension_values[0].value, row.dimension_values[1].value.encode('utf-8'), row.metric_values[0].value])
+        report.append([row.dimension_values[0].value, row.dimension_values[1].value.encode('utf-8'), row.metric_values[0].value])
     GCS_PATH = os.environ['GCS_PATH']
-    upload_blob('./popular.csv', GCS_PATH + 'popular.csv')
+    bucket = storage_client.bucket(os.environ['BUCKET'])
+    upload_data(bucket, report, str, GCS_PATH + 'popular.csv')
 
+def upload_data(bucket_name: str, data: bytes, content_type: str, destination_blob_name: str):
+    '''Uploads a file to the bucket.'''
+    # bucket_name = 'your-bucket-name'
+    # data = 'storage-object-content'
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    # blob.content_encoding = 'gzip'
+    blob.upload_from_string(
+        # data=gzip.compress(data=data, compresslevel=9),
+        data=bytes(data.encode('utf-8')),
+        content_type=content_type, client=storage_client)
+    blob.content_language = 'zh'
+    blob.cache_control = 'max-age=300,public'
+    blob.patch()
 
 def upload_blob(source, destination_file):
     storage_client = storage.Client()
