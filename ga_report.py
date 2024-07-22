b4bc7831229273270372eb2a4e162a21007d85d3
hcchien <hcchien@gmail.com>
2024-07-22 13:21:28 +0800

    reduce the popular period

--- ga_report.py
+++ ga_report.py
@@ -87,7 +87,7 @@ def popular_report(property_id, dest_file='popular.json', extra=''):
     client = BetaAnalyticsDataClient()
 
     current_time = datetime.now()
-    start_datetime = current_time - timedelta(days=3)
+    start_datetime = current_time - timedelta(days=2)
     start_date = datetime.strftime(start_datetime, '%Y-%m-%d')
 
     request = RunReportRequest(
