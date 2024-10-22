50a89d66818cdcd4e1ff5ebf64394035b3fbf799
Dylan Liu <gaespa007648@gmail.com>
2024-10-22 11:52:38 +0800

    feat: add recent_popular_report
    If we need to parse the publishedDate within one day,
    we should modify the pipeline of popular_report by first fetching the recent posts then second get ga-analytics-data for each post.

--- ga_report.py
+++ ga_report.py
@@ -16,7 +16,7 @@ from google.analytics.data_v1beta.types import Metric
 from google.analytics.data_v1beta.types import RunReportRequest
 from datetime import datetime
 
-def get_article(article_ids, extra='', days: int=1):
+def get_article(article_ids, extra=''):
     GQL_ENDPOINT = os.environ['GQL_ENDPOINT']
     gql_transport = AIOHTTPTransport(url=GQL_ENDPOINT)
     gql_client = Client(transport=gql_transport,
@@ -76,7 +76,7 @@ def get_article(article_ids, extra='', days: int=1):
         #report.append({'title': row.dimension_values[0].value, 'uri': row.dimension_values[1].value, 'count': row.metric_values[0].value})
     return report
 
-def popular_report(property_id, dest_file='popular.json', extra='', ga_days: int=2, published_days: int=1):
+def popular_report(property_id, dest_file='popular.json', extra='', ga_days: int=2):
     """Runs a simple report on a Google Analytics 4 property."""
     # TODO(developer): Uncomment this variable and replace with your
     #  Google Analytics 4 property ID before running the sample.
@@ -105,11 +105,107 @@ def popular_report(property_id, dest_file='popular.json', extra='', ga_days: int
     print("report result")
     print(response)
 
-    report = get_article(response.rows, extra, published_days)
+    report = get_article(response.rows, extra)
     gcs_path = os.environ['GCS_PATH']
     bucket = os.environ['BUCKET']
     upload_data(bucket, json.dumps(report, ensure_ascii=False).encode('utf8'), 'application/json', gcs_path + dest_file)
     return "Ok"
+
+def recent_popular_report(property_id, dest_file='recent_popular.json', days: int=1):
+    # env and config
+    POPULAR_POSTS_NUM = 30
+    GQL_ENDPOINT = os.environ['GQL_ENDPOINT']
+    gcs_path = os.environ['GCS_PATH']
+    bucket   = os.environ['BUCKET']
+    
+    # get recent posts
+    if sys.stdout:
+        sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+    current_time = datetime.now()
+    start_datetime = (current_time - timedelta(days=days)).replace(hour=0, minute=0, second=0, microsecond=0)
+    filter_publishedDate = f"\"{start_datetime.isoformat(timespec='seconds')}Z\""
+    
+    
+    gql_transport = AIOHTTPTransport(url=GQL_ENDPOINT)
+    gql_client = Client(transport=gql_transport,
+                        fetch_schema_from_transport=False)
+    posts_gql = '''
+        query{{
+            posts(where: {{publishedDate: {{gt: {START_DATE} }} }}){{
+                id
+                slug
+                sections{{id, name, slug, state}}
+                sectionsInInputOrder{{id, name, slug, state}}
+                title
+                style
+                state
+                publishedDate
+                heroImage{{
+                    id, 
+                    resized{{
+                        original
+                        w480,
+                        w800,
+                        w1200,
+                        w1600,
+                        w2400
+                    }},
+                    resizedWebp{{
+                        original
+                        w480,
+                        w800,
+                        w1200,
+                        w1600,
+                        w2400
+                    }}
+                }}
+            }}
+        }}
+    '''.format(START_DATE=filter_publishedDate)
+    data = gql_client.execute(gql(posts_gql))
+    
+    # organize posts
+    posts = data['posts']
+    posts_table = {
+        f"/story/{post['slug']}": post for post in posts
+    }
+    filter_slugs = list(posts_table.keys())
+    
+    # fetch ga-analytics data
+    start_date = datetime.strftime(start_datetime, '%Y-%m-%d')
+    request = RunReportRequest(
+        property=f"properties/{property_id}",
+        dimensions=[
+            Dimension(name="pagePath")
+        ],
+        metrics=[Metric(name="screenPageViews")],
+        date_ranges=[DateRange(start_date=start_date, end_date="today")],
+        dimension_filter={
+            "filter": {
+                "field_name": "pagePath",
+                "in_list_filter": {
+                    "values": filter_slugs,
+                }
+            }
+        }
+    )
+    client = BetaAnalyticsDataClient()
+    response = client.run_report(request)
+    
+    # organize result
+    rows = response.rows
+    statistic_data = {row.dimension_values[0].value: int(row.metric_values[0].value) for row in rows}
+    sorted_statistic_data = sorted(statistic_data.items(), key=lambda item: item[1], reverse=True)[:POPULAR_POSTS_NUM]
+    
+    report = []
+    for data in sorted_statistic_data:
+        slug = data[0]
+        post = posts_table[slug]
+        report.append(post)
+    
+    # upload
+    upload_data(bucket, json.dumps(report, ensure_ascii=False).encode('utf8'), 'application/json', gcs_path + dest_file)
+    return "ok"
 
 def upload_data(bucket_name: str, data: str, content_type: str, destination_blob_name: str):
     '''Uploads a file to the bucket.'''
